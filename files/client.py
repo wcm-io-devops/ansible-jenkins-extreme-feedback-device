acc3b0458ed89659832131d0ab23fde7f1ae49a7
Tobias Richter <trichter@pro-vision.de>
2020-01-28 17:18:28 +0100

    Keep last payload to maintain build status on service restart, render received and rendered time into template, add link to github project

--- files/client.py
+++ files/client.py
@@ -4,24 +4,45 @@ import paho.mqtt.client as mqtt
 import systemd.daemon
 import yaml
 import jinja2
+import datetime
+import os.path
 
 # change to dir of the client script
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+last_payload_file = "last_payload.yml"
+
 def on_message(mqttc, obj, msg):
     payload = msg.payload.decode("utf-8")
-    yml_payload = yaml.load(payload, Loader=yaml.SafeLoader)
-    print("Got message %s" % yml_payload)
+    try:
+        yml_payload = yaml.load(payload, Loader=yaml.SafeLoader)
+        print("Got payload %s" % yml_payload)
+        # store message to keep status on service restart
+        f = open(last_payload_file, "w")
+        yaml.dump(yml_payload, f)
+        f.close()
 
-    BUILD_NUMBER = yml_payload.get('BUILD_NUMBER', None)
-    BUILD_RESULT = yml_payload.get('BUILD_RESULT', None)
-    BUILD_RESULT_COLOR = yml_payload.get('BUILD_RESULT_COLOR', '#000000')
-    BUILD_URL = yml_payload.get('BUILD_URL', None)
-    JENKINS_URL = yml_payload.get('JENKINS_URL', None)
-    JOB_BASE_NAME = yml_payload.get('JOB_BASE_NAME', None)
-    JOB_DISPLAY_URL = yml_payload.get('JOB_DISPLAY_URL', None)
-    JOB_NAME = yml_payload.get('JOB_NAME', None)
-    RUN_CHANGES_DISPLAY_URL = yml_payload.get('RUN_CHANGES_DISPLAY_URL', None)
+        handle_payload(yml_payload)
+    except:
+        print("Unable to parse MQTT message as yaml.")
+
+def handle_payload(payload):
+    if not payload:
+        print("No valid payload received")
+        return
+
+    BUILD_NUMBER = payload.get('BUILD_NUMBER', None)
+    BUILD_RESULT = payload.get('BUILD_RESULT', None)
+    BUILD_RESULT_COLOR = payload.get('BUILD_RESULT_COLOR', '#000000')
+    BUILD_URL = payload.get('BUILD_URL', None)
+    JENKINS_URL = payload.get('JENKINS_URL', None)
+    JOB_BASE_NAME = payload.get('JOB_BASE_NAME', None)
+    JOB_DISPLAY_URL = payload.get('JOB_DISPLAY_URL', None)
+    JOB_NAME = payload.get('JOB_NAME', None)
+    RUN_CHANGES_DISPLAY_URL = payload.get('RUN_CHANGES_DISPLAY_URL', None)
+    MESSAGE_TIMESTAMP = int(payload.get('TIMESTAMP', 0))
+    MESSAGE_TIME = datetime.datetime.fromtimestamp(MESSAGE_TIMESTAMP)
+    RENDER_TIME = datetime.datetime.now()
 
     JOB_BASE_NAME = JOB_BASE_NAME.replace("%2F", "/")
 
@@ -35,7 +56,12 @@ def on_message(mqttc, obj, msg):
         JOB_BASE_NAME=JOB_BASE_NAME,
         JOB_DISPLAY_URL=JOB_DISPLAY_URL,
         JOB_NAME=JOB_NAME,
-        RUN_CHANGES_DISPLAY_URL=RUN_CHANGES_DISPLAY_URL)
+        RUN_CHANGES_DISPLAY_URL=RUN_CHANGES_DISPLAY_URL,
+        RENDER_TIME=RENDER_TIME,
+        MESSAGE_TIME=MESSAGE_TIME,
+        MESSAGE_TIMESTAMP=MESSAGE_TIMESTAMP,
+    )
+
     f = open("/var/www/html/index.html", "w")
     f.write(rendered_template)
     f.close()
@@ -46,7 +72,6 @@ def on_message(mqttc, obj, msg):
         setLight([1, 0, 0])
     elif BUILD_RESULT == "UNSTABLE" or BUILD_RESULT == "STILL UNSTABLE":
         setLight([0, 1, 0])
-
 
 def setLight(lights):
     print("setLight: %s" % (lights))
@@ -93,17 +118,32 @@ topics = mqttCfg['topics']
 host = mqttCfg['host']
 device_sn = cfg['device_sn']
 
-print("Initializing lights")
-setLight([0,0,0])
-
 # we could also run .subscribe multiple times, but this is not recommended. Therefor we build tuples of (topic, qos)
 tps = [(topic, 0) for topic in topics]
 
 mqttc = mqtt.Client()
+
+# load last message yaml_file when it exists
+if (os.path.exists(last_payload_file)):
+    print("Loading status from %s." % (last_payload_file))
+    #try:
+    file = open(last_payload_file, "r")
+    last_payload = yaml.load(file, Loader=yaml.SafeLoader)
+    print("last_payload %s." % (last_payload))
+    handle_payload(last_payload)
+    #except:
+    #    print("Unable to load and parse %s as yaml" % (last_payload_file))
+
+else:
+    print("Initializing lights")
+    setLight([0,0,0])
+
 mqttc.on_message = on_message
 mqttc.connect(host, 1883, 60)
 
 mqttc.subscribe(tps)
+
+
 
 # notify systemd that the service is ready
 systemd.daemon.notify('READY=1')
